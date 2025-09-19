fc046fd165ccdcea0583e10990c9ba8bf4e7f82b
amanda-bann <amanda.banner@gmail.com>
2025-09-19 14:51:21 -0600

    Create state and set character limit for message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [message, setMessage] = useState("");
+// Could handle this inline, but extracting to a function for clarity
+  const handleOnChange = (e) => {
+    setMessage(e.target.value)
+  };
+  const [showSuccess, setShowSuccess] = useState(false);
+  const maxLength = 60;
+
   return (
     <div className="App">
       <div className="status-update">
@@ -10,7 +18,14 @@ export default function App() {
           <label htmlFor="status">
             Tell us how you are doing in 60 characters or less!
           </label>
-          <textarea id="status" placeholder="I'm crushing it!" />
+          <textarea 
+            id="status" 
+            placeholder="I'm crushing it!"
+            value={message} 
+            onChange={handleOnChange} 
+            //Inline function could be used here for onChange instead.
+            // For example: onChange={(e) => setMessage(e.target.value)}//
+            />
           <button type="submit">Tell the world!</button>
         </form>
       </div>
