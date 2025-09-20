a72a33f112b11b4e635abcecb9232e5aa0e573a6
amanda-bann <amanda.banner@gmail.com>
2025-09-19 20:12:17 -0600

    Submit the form and show the user a success message

--- src/App.js
+++ src/App.js
@@ -10,26 +10,35 @@ export default function App() {
   const [showSuccess, setShowSuccess] = useState(false);
   const maxLength = 60;
 
+  const handleSubmit = (e) => {
+    e.preventDefault ();
+    setShowSuccess(true)
+  }
+
   return (
     <div className="App">
-      <div className="status-update">
-        <h1>Super Short Status!</h1>
-        <form>
+      {showSuccess ? (
+        <p className="message-success">Awesome! Thanks for the update.</p>
+      ) : (
+        <div className="status-update">
+          <h1>Super Short Status!</h1>
+          <form onSubmit={handleSubmit}>
           <label htmlFor="status">
             Tell us how you are doing in 60 characters or less!
           </label>
-          <textarea 
-            className={message.length > maxLength ? "too-long" : ""}
-            id="status" 
-            placeholder="I'm crushing it!"
-            value={message} 
-            onChange={handleOnChange} 
-            //Inline function could be used here for onChange instead.
-            // For example: onChange={(e) => setMessage(e.target.value)}//
+            <textarea 
+              className={message.length > maxLength ? "too-long" : ""}
+              id="status" 
+              placeholder="I'm crushing it!"
+              value={message} 
+              onChange={handleOnChange} 
+              //Inline function could be used here for onChange instead.
+              // For example: onChange={(e) => setMessage(e.target.value)}//
             />
-          <button disabled={message.length > maxLength} type="submit">Tell the world!</button>
-        </form>
-      </div>
+            <button disabled={message.length > maxLength} type="submit">Tell the world!</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
